b5ecd792895788bb9da0e4614d1b2b093f3a88ef
Dirk Watkins <dirk.watkins@perficient.com>
2023-02-06 10:05:19 -0600

    print update history to console

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿//See https://aka.ms/new-console-template for more information on the new simple console template.
 
+using ConsoleTables;
 using GuitarShop;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,56 +8,87 @@ Console.WriteLine("GUITAR STORE");
 Console.WriteLine();
 
 
-var dirksId = new Guid("9e105ebb-5e0e-450f-8a37-f664d40be1c1"); 
+var dirksId = new Guid("9e105ebb-5e0e-450f-8a37-f664d40be1c1");
 var stratId = new Guid("85b25108-1423-4f63-ad8e-7b823d5f8344");
+using var db = new StoreContext();
 
-//using (var db = new StoreContext())
+
+//initialize the data
+var dirk = db.Musicians.Include(x => x.Guitars).FirstOrDefault(x => x.Id == dirksId);
+var strat = db.Guitars.FirstOrDefault(x => x.Id == stratId);
+
+if (strat == null)
+{
+    strat = new Guitar(stratId, "Fender", "Stratocaster", "Black", 1000);
+    db.Guitars.Add(strat);
+}
+
+if (dirk == null)
+{
+    dirk = new Musician(dirksId, "Dirk Watkins");
+    db.Musicians.Add(dirk);
+    dirk.Guitars.Add(strat);
+}
+
+db.SaveChanges();
+
+Console.WriteLine($"GUITARS FOR: {dirk.Name}");
+foreach (var guitar in dirk.Guitars)
+{
+    Console.WriteLine($" - {guitar.Make} {guitar.Model} {guitar.Color} {guitar.Price:c0}");
+}
+
+
+//Let's update the data so we can show some history in the temporal table logs
+//separated by a second so we differentiate them in the db
+
+//strat.Color = "Blue Crayon";
+//db.SaveChanges();
+
+//Task.Delay(1000);
+//strat.Color = "Red Sharpie";
+//db.SaveChanges();
+
+//Task.Delay(1000);
+//strat.Color = "Blue Splash";
+//db.SaveChanges();
+
+////the data will be updated to the latest 
+//Console.WriteLine();
+//Console.WriteLine("AFTER UPDATES:");
+
+//foreach (var guitar in dirk.Guitars)
 //{
-//    var strat = new Guitar(stratId, "Fender", "Stratocaster", "Black", 1000);
-//    db.Guitars.Add(strat);
-
-//    var dirk = new Musician(dirksId, "Dirk Watkins");
-//    dirk.Guitars.Add(strat);
-//    db.Musicians.Add(dirk);
-
-//    db.SaveChanges();
+//    Console.WriteLine($" - {guitar.Make} {guitar.Model} {guitar.Color} {guitar.Price:c0}");
 //}
 
 
-//using (var db = new StoreContext())
-//{
-//    var dirk = db.Musicians.Include(x => x.Guitars).FirstOrDefault(x => x.Id == dirksId);
-//    Console.WriteLine($"Guitars for {dirk.Name}");
-//    foreach (var guitar in dirk.Guitars)
-//    {
-//        Console.WriteLine($" - {guitar.Make} {guitar.Model} {guitar.Color} {guitar.Price:c0}");
-//    }
-    
-//    var strat = db.Guitars.FirstOrDefault(x => x.Id == stratId);
-//    strat.Color = "Blue Crayon";
-//    db.SaveChanges();
+//now we can show some history!
+//note: if you run this a bunch of times you'll see a bunch of logs!
+Console.WriteLine();
+Console.WriteLine("GUITAR UPDATE HISTORY:");
 
-//    Task.Delay(1000);
+//project to an anonymous type so we can have the valid from/to shadow properties
+var guitarSnapshots = await db.Guitars
+    .TemporalAll()
+    .OrderByDescending(x => EF.Property<DateTime>(x, "ValidFrom"))
+    .Where(x => x.Id == stratId)
+    .Select(x => new
+    {
+        ValidFrom = EF.Property<DateTime>(x, "ValidFrom"),
+        ValidTo = EF.Property<DateTime>(x, "ValidTo"),
+        x.Make,
+        x.Model,
+        x.Color,
+        x.Price,
+    })
+    .ToListAsync();
 
-//    strat.Color = "Red Sharpie";
-//    db.SaveChanges();
-
-
-//    Task.Delay(1000);
-
-//    strat.Color = "Blue Splash";
-//    db.SaveChanges();
-
-//    Console.WriteLine("---- AFTER:");
-
-//    foreach (var guitar in dirk.Guitars)
-//    {
-//        Console.WriteLine($" - {guitar.Make} {guitar.Model} {guitar.Color} {guitar.Price:c0}");
-//    }
-//}
-
+//write out a table with this fancy nuget package
+ConsoleTable.From(guitarSnapshots).Write(Format.Minimal);
 
 
 //Pause the program
-Console.WriteLine(" - DONE - ");
+Console.WriteLine();
+Console.WriteLine("DONE");
 Console.ReadKey();
